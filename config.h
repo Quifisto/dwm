10b593462af01249b25f039dc6e5ca9c1ed911ea
quifisto <finten@hotmail.nl>
2020-01-14 14:21:17 +0100

    Added rofi command SUPER+P. Colors matching background. Firefox icon. Added scrolling configuration. Changed lots of shortcuts to SUPER

--- config.h
+++ config.h
@@ -1,19 +1,19 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int horizbadbar        = 1;        /* These add padding around the text in the bar */
+static const int horizbadbar        = 0;        /* These add padding around the text in the bar */
 static const int vertpadbar         = 2;
-static const char *fonts[]          = { "monospace:size=10", "Font Awesome 5 Free Regular:size=14", "Font Awesome 5 Brands:size=14" };
+static const char *fonts[]          = { "monospace:size=10", "Font Awesome 5 Free Regular:size=12", "Font Awesome 5 Brands:size=12" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#2b203c";  // dark purple
+static const char col_gray2[]       = "#444444";  // grey
+static const char col_gray3[]       = "#f8f4c0";  // off white
+static const char col_gray4[]       = "#ffa000";  // deep yellow
+static const char col_cyan[]        = "#aa2637";  // bordeau
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -52,10 +52,10 @@ static const Layout layouts[] = {
 #define MODKEY Mod1Mask
 #define SUPER Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ SUPER,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ SUPER|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ SUPER|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ SUPER|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -63,6 +63,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *roficmd[] = {"rofi", "-modi", "drun,run", "-show", "drun", "-show-icons"};
 static const char *termcmd[]  = { "st", NULL };
 static const char *floattermcmd[] = { "st", "-t", "float-term", NULL };
 static const char *browsercmd[] = { "firefox", NULL };
@@ -74,8 +75,9 @@ static const char *mutevol[] = { "amixer", "set", "Master", "toggle",  NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+        { SUPER,                        XK_p,      spawn,          {.v = roficmd } },
 	{ SUPER,                        XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_grave,  spawn,          {.v = floattermcmd } },
+	{ SUPER,                       XK_grave,  spawn,          {.v = floattermcmd } },
 	{ SUPER,                        XK_w,      spawn,          {.v = browsercmd } },
 	{ SUPER,                        XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -87,13 +89,13 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ SUPER,                        XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ SUPER,                        XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ SUPER,                        XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ SUPER,                        XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ SUPER,                        XK_space,  setlayout,      {0} },
+	{ SUPER|ShiftMask,              XK_space,  togglefloating, {0} },
+	{ SUPER,                        XK_0,      view,           {.ui = ~0 } },
+	{ SUPER|ShiftMask,              XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
@@ -122,12 +124,12 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         SUPER,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         SUPER,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         SUPER,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkTagBar,            SUPER,         Button1,        tag,            {0} },
+	{ ClkTagBar,            SUPER,         Button3,        toggletag,      {0} },
 };
 
